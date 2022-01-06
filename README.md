61732a188e0b6e70f305f4f719cb940dd99c8f10
Drew Linsley <drewlinsley@gmail.com>
2022-01-06 09:56:04 -0500

    Updated interface to use yml configs.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Install required packages with `pip install -r requirements.txt`.
+1. Install required packages with `pip install -r requirements.txt`.
 
-## The project works off of ".yml" config files, which hold information about your project. Create one of these per project.
+2. The project works off of ".yml" config files, which hold information about your project. Create one of these per project.
 
-## Run a script like `python split_merge.py configs/W-Q.yml`
+Run a script using: `python split_merge.py configs/W-Q.yml`
 
--- split_merge.py
+++ split_merge.py
@@ -10,6 +10,7 @@ from omegaconf import OmegaConf
 
 
 def main(conf):
+    import pdb;pdb.set_trace()
     token = conf.token
     path = conf.revision_path  # WQ
     scale = conf.scale  # (5.0, 5.0, 50.0)
@@ -31,76 +32,76 @@ def main(conf):
             mags=[Mag("1")],
             path=path,
         )
-    annotation = wk.Annotation.download(annotation_url)
-    time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
-    new_dataset_name = annotation.dataset_name + f"_segmented_{time_str}"
-    new_dataset = wk.Dataset(new_dataset_name, scale=scale)
+        annotation = wk.Annotation.download(annotation_url)
+        time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+        new_dataset_name = annotation.dataset_name + f"_segmented_{time_str}"
+        new_dataset = wk.Dataset(new_dataset_name, scale=scale)
 
-    # Load skeletons (ideally get this through annotation api in the future?)
-    nml = annotation.skeleton
+        # Load skeletons (ideally get this through annotation api in the future?)
+        nml = annotation.skeleton
 
-    # Start merging annotations into base
-    print("Loading data into memory")
-    volume_annotation_layer = annotation.save_volume_annotation(new_dataset)
-    print(volume_annotation_layer.bounding_box)
-    volume_annotation_mag = volume_annotation_layer.mags[wk.Mag(1)]
+        # Start merging annotations into base
+        print("Loading data into memory")
+        volume_annotation_layer = annotation.save_volume_annotation(new_dataset)
+        print(volume_annotation_layer.bounding_box)
+        volume_annotation_mag = volume_annotation_layer.mags[wk.Mag(1)]
 
-    # Overwrite base annotation with new annotations
-    segmentation_mag = dataset.layers["segmentations"].mags[wk.Mag(1)]
-    segmentation_data = segmentation_mag.read()
-    print("Overwriting base annotation. This could take a while.")
-    for offset, size in volume_annotation_mag.get_bounding_boxes_on_disk():
-        data = volume_annotation_mag.read(offset, size)
-        segmentation_data[
-            0,
-            offset[0]: offset[0] + size[0],
-            offset[1]: offset[1] + size[1],
-            offset[2]: offset[2] + size[2]]
-        segmentation_mag.write(data, offset)
-        print(f"Overwrote bbox {offset} {size}")
+        # Overwrite base annotation with new annotations
+        segmentation_mag = dataset.layers["segmentations"].mags[wk.Mag(1)]
+        segmentation_data = segmentation_mag.read()
+        print("Overwriting base annotation. This could take a while.")
+        for offset, size in volume_annotation_mag.get_bounding_boxes_on_disk():
+            data = volume_annotation_mag.read(offset, size)
+            segmentation_data[
+                0,
+                offset[0]: offset[0] + size[0],
+                offset[1]: offset[1] + size[1],
+                offset[2]: offset[2] + size[2]]
+            segmentation_mag.write(data, offset)
+            print(f"Overwrote bbox {offset} {size}")
   
-    # Merge through skeletions
-    edits = {}  # A dict with a list per entry
-    for tree in nml._children:  # nml.trees() is a flattened iterator of all trees
-        name_str = tree.graph["name"]
-        name = name_str.split("_")[0]
-        command = name_str.split("_")[1]
-        coords = [np.asarray(x.position).astype(int) for x in tree.nodes]
-        if len(coords):
-            segto = segmentation_data[0, coords[0][0], coords[0][1], coords[0][2]]
-            segfrom = segmentation_data[0, coords[1][0], coords[1][1], coords[1][2]]
-            if name not in edits:  # Sort the commands into different edits
-                edits[name] = {}
-            edits[name][command] = [{segfrom: segto}, coords]
-    for name, commands in tqdm(edits.items(), total=len(edits), desc="Merging"):
-        pos = commands["merge"]
-        segmentation_data = fastremap.remap(segmentation_data, pos[0], preserve_missing_labels=True)
-        if len(commands) == 2:
-            # Pos (merge) and neg (split) control
-            neg = commands["split"]
-            negfrom = segmentation_data[0, neg[1][0][0], neg[1][0][1], neg[1][0][2]]
-            negto = segmentation_data[0, neg[1][1][0], neg[1][1][1], neg[1][1][2]]
-            if negfrom == negto:
-                print("Segment id bled from {} to {} in negative control.".format(neg[1][0], neg[1][1]))
-            else:
-                pos_0 = [x for x in pos[0].keys()][1]
-                print("Segment id {} successfully propogated.".format(pos_0))
-            negid = neg.values()[1]
+        # Merge through skeletions
+        edits = {}  # A dict with a list per entry
+        for tree in nml._children:  # nml.trees() is a flattened iterator of all trees
+            name_str = tree.graph["name"]
+            name = name_str.split("_")[0]
+            command = name_str.split("_")[1]
+            coords = [np.asarray(x.position).astype(int) for x in tree.nodes]
+            if len(coords):
+                segto = segmentation_data[0, coords[0][0], coords[0][1], coords[0][2]]
+                segfrom = segmentation_data[0, coords[1][0], coords[1][1], coords[1][2]]
+                if name not in edits:  # Sort the commands into different edits
+                    edits[name] = {}
+                edits[name][command] = [{segfrom: segto}, coords]
+        for name, commands in tqdm(edits.items(), total=len(edits), desc="Merging"):
+            pos = commands["merge"]
+            segmentation_data = fastremap.remap(segmentation_data, pos[0], preserve_missing_labels=True)
+            if len(commands) == 2:
+                # Pos (merge) and neg (split) control
+                neg = commands["split"]
+                negfrom = segmentation_data[0, neg[1][0][0], neg[1][0][1], neg[1][0][2]]
+                negto = segmentation_data[0, neg[1][1][0], neg[1][1][1], neg[1][1][2]]
+                if negfrom == negto:
+                    print("Segment id bled from {} to {} in negative control.".format(neg[1][0], neg[1][1]))
+                else:
+                    pos_0 = [x for x in pos[0].keys()][1]
+                    print("Segment id {} successfully propogated.".format(pos_0))
+                negid = neg.values()[1]
 
-    # Overwrite annotations
-    dataset.delete_layer(segmentation_layer)
-    segmentation_layer = dataset.add_layer(
-        segmentation_layer,
-        wk.SEGMENTATION_CATEGORY,
-        volume_annotation_layer.dtype_per_channel,
-        largest_segment_id=segmentation_data.max()
-    )
-    segmentation_mag = segmentation_layer.add_mag("1")
-    segmentation_mag.write(dsegmentation_data)
-    segmentation_mag.compress()
-    segmentation_layer.downsample()
-    url = dataset.upload()
-    print("Uploaded new base annotation to: {}".format(url)) 
+        # Overwrite annotations
+        dataset.delete_layer(segmentation_layer)
+        segmentation_layer = dataset.add_layer(
+            segmentation_layer,
+            wk.SEGMENTATION_CATEGORY,
+            volume_annotation_layer.dtype_per_channel,
+            largest_segment_id=segmentation_data.max()
+        )
+        segmentation_mag = segmentation_layer.add_mag("1")
+        segmentation_mag.write(dsegmentation_data)
+        segmentation_mag.compress()
+        segmentation_layer.downsample()
+        url = dataset.upload()
+        print("Uploaded new base annotation to: {}".format(url)) 
 
 
 if __name__ == '__main__':
